c441b338b81e5588685d669c4668358f3f764a41
phuonguyenkase <phuonguyen19042905@gmail.com>
2024-06-15 16:13:05 +0700

    Pracetic4.sql

---
+++ Pracetic4.sql
@@ -0,0 +1,23 @@
+#Ex1:
+SELECT 
+SUM(CASE
+WHEN device_type = 'laptop' then 1 else 0
+END) as laptop_views,
+SUM(CASE
+WHEN device_type != 'laptop' then 1 else 0
+END) as mobile_views
+FROM viewership;
+
+#Ex2:
+SELECT *, 
+CASE WHEN (x+y>z and y+z>x and z+x>y)
+THEN 'Yes'
+Else 'No'
+End as triangle
+FROM Triangle;
+
+#Ex3:
+SELECT ROUND(
+    CAST(SUM(CASE WHEN COALESCE(call_category, 'n/a') = 'n/a' THEN 1 ELSE 0 END) AS DECIMAL) / 
+    CAST(COUNT(*) AS DECIMAL) * 100, 1) AS uncategorised_call_pct
+FROM callers;
