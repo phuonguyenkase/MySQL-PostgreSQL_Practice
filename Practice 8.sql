32b89c8b53e64f26bc973c20d7b497ed279daf8c
Kase <phuonguyen19042905@gmail.com>
2024-06-28 00:06:15 +0700

    Practice 8.sql

--- Practice 8.sql
+++ Practice 8.sql
@@ -9,3 +9,15 @@ SELECT ROUND(100*COUNT(type_delivery)/(SELECT COUNT(type_delivery) FROM diff WHE
 FROM diff
 WHERE ranking = 1
 AND type_delivery = 'immediate';
+
+#Ex2:
+WITH day_diff AS (
+SELECT player_id, event_date,
+LEAD(event_date) OVER (PARTITION BY player_id ORDER BY event_date) AS prev_date,
+ROW_NUMBER() OVER(PARTITION BY player_id ORDER BY event_date) AS ranking
+FROM Activity)
+
+SELECT ROUND(COUNT(DISTINCT player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) AS fraction
+FROM day_diff
+WHERE DATEDIFF(prev_date, event_date) = 1
+AND ranking = 1;
