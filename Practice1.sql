9a8cebb45dd37f48ff1c96e64137075bc96d9c77
Kase <phuonguyen19042905@gmail.com>
2025-01-07 20:46:33 +0700

    Practice1.sql

--- Practice1.sql
+++ Practice1.sql
@@ -21,6 +21,7 @@ OR CITY LIKE 'e%'
 OR CITY LIKE 'i%'
 OR CITY LIKE 'o%'
 OR CITY LIKE 'u%';
+#Hoặc dùng: WHERE city REGEXP '^[aeiou]' còn postgresql thì WHERE city ~ '^[aeiou]'
 
 #Ex5 (tương tự):
 SELECT Distinct City
